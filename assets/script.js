fd94db035ca3c2dc54285ebea964cc85c3ef3630
Alex Hannani <alexhannani24@gmail.com>
2022-12-12 22:23:27 -0500

    added the variables for current weather in the js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
 // Variables at the global level
-
 var apiKey = "d869eb6dae6960fe226dbf67bb6b6b4b"
+
+// The variables for the current weather
+var cityArray = [];
+var currentWind = $("#wind");
+var currentHumidity = $("#humidity");
+var currentTemp = $("#temp");
+var currentCity = $("#currentCity");
+
+var searchHistory = $(".searchHistory")
